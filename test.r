f1a29ad7806efa6105a9468b4e4c7c88c7c7df9e
Matt Schultz <matt@mwschultz.com>
2017-08-08 10:17:41 -0400

    Message

--- test.r
+++ test.r
@@ -1,1 +1,17 @@
-3*5
+require(tidyverse)
+require(haven)
+require(dplyr)
+require(Lahman)
+require(magrittr)
+
+Batting %>% select(X2B:HR)
+
+c<-Batting %>% transmute(ExtraBaseHits = X2B + X3B + HR)
+
+#mutate -- add newly created column to data frame 
+#transmute -- create new variable 
+
+Batting %>% summarise(AvgX2B = mean(X2B, na.rm=TRUE)) 
+#NA is missing 
+
+Batting %>% group_by (teamID)
